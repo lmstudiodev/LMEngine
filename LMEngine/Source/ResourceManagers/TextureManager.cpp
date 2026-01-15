41b66d5ec81db07be56255ac9685106e09dc425a
lmstudiodev <lmstudiodev@gmail.com>
2026-01-15 12:17:12 +0100

    Added new texture creation method.

--- LMEngine/Source/ResourceManagers/TextureManager.cpp
+++ LMEngine/Source/ResourceManagers/TextureManager.cpp
@@ -15,6 +15,23 @@ TexturePtr TextureManager::CreateTextureFromFile(const wchar_t* file_path)
 	return std::static_pointer_cast<Texture>(CreateResourceFromFile(file_path));
 }
 
+TexturePtr TextureManager::CreateTexture(const Rect& size, Texture::Type type)
+{
+	Texture* text = nullptr;
+
+	try
+	{
+		text = new Texture(size, type);
+	}
+	catch (...)
+	{
+	}
+
+	TexturePtr texture(text);
+
+	return texture;
+}
+
 Resource* TextureManager::CreateResourceFromFileConcrete(const wchar_t* file_path)
 {
 	Texture* text = nullptr;
